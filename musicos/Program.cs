677260b5f8d13c9e2895d81384b7030839266c31
Iker Alonso <126517793+AIkerVSaul09@users.noreply.github.com>
2023-03-22 19:57:58 +0000

    Musico

---
+++ musicos/Program.cs
@@ -0,0 +1,74 @@
+﻿class Musico
+{
+    public string Nombre {get;set;}
+
+    public Musico(string nombre)
+    {
+        Nombre=nombre;
+    }
+
+    public void Saludar()
+    {
+      Console.WriteLine($"Hola soy {Nombre}");
+    }
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando un instrumento");
+    }
+
+}
+
+class Baterista:Musico
+{
+    public string bateria{get;set;}
+
+ public Baterista(string nombre, string b):base(nombre)
+ {
+    bateria=b;
+ }
+    public override void Toca()
+    {
+        Console.WriteLine($"Tocando su {bateria} el artista {Nombre}");
+    }
+
+}
+
+class Bajista:Musico
+{
+ public string bajo{get;set;}
+
+ public Bajista(string nombre, string b):base(nombre)
+ {
+    bajo=b;
+ }
+    public override void Toca()
+    {
+        Console.WriteLine($"Tocando su {bajo} el artista {Nombre}");
+    }
+
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Musico EdSheeran = new Musico("EdSheeran");
+        EdSheeran.Saludar();
+        EdSheeran.Toca();*/
+
+        List<Musico> grupo= new List<Musico>();
+        grupo.Add(new Bajista("joe","Yamanaka"));
+        grupo.Add(new Baterista("Eric","Mac"));
+        grupo.Add(new Musico("EdSheeran"));
+
+        foreach(var m in grupo)
+        {
+          m.Saludar();
+        }
+        foreach(var m in grupo)
+        {
+          m.Toca();
+        }
+
+    }
+}
